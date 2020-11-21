2a922d93626aa3e3c855182255fc4244b6b0fc1b
Aswini0721 <60321675+Aswini0721@users.noreply.github.com>
2020-11-21 23:47:06 +0530

    Sample Sprint Boot Project

---
+++ sample-mg/src/main/java/com/sample/Application.java
@@ -0,0 +1,19 @@
+package com.sample;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+@EnableScheduling
+public class Application extends SpringBootServletInitializer {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
