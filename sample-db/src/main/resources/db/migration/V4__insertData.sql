58590e0b04303e9ad5f4324db1476177731cd77f
sangamaeswaran_t <sangamaeswaran_t@trigent.com>
2020-11-30 23:39:26 +0530

    Commited the DB

---
+++ sample-db/src/main/resources/db/migration/V4__insertData.sql
@@ -0,0 +1,2 @@
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('test', 'java', 'test','a@gmail.com','9910121314');
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('bh', 'basant', 'hota','b@gmail.com','7735888844');
