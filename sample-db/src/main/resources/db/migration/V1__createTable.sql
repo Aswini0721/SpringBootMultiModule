4a70986ffbbec3436fce0db0e9cdaeeece852058
Aswini0721 <aswini.sreepathi@gmail.com>
2021-01-08 15:02:31 +0530

    Initial COmmit

--- sample-db/src/main/resources/db/migration/V1__createTable.sql
+++ sample-db/src/main/resources/db/migration/V1__createTable.sql
@@ -1,9 +1,7 @@
 CREATE TABLE REGISTRATION_USERS (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
+  id         int not null auto_increment primary key,
   username varchar(100) NOT NULL,
   first_name varchar(50) NOT NULL,
   last_name varchar(50) DEFAULT NULL,
-  email varchar(50) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY UK_username (username)
-)
+  email varchar(50) NOT NULL
+);
--- sample-db/src/main/resources/db/migration/V4__insertData.sql
+++ sample-db/src/main/resources/db/migration/V4__insertData.sql
@@ -1,2 +1,3 @@
 INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('test', 'java', 'test','a@gmail.com','9910121314');
 INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('bh', 'basant', 'hota','b@gmail.com','7735888844');
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('ABC', 'DEF', 'hota','abc@gmail.com','7735888844');
